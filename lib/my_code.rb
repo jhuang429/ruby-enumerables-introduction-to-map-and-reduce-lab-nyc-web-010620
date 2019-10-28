6aaaa967bd131d96d5a28010c6aa68c1fb61b1c1
Jerry Huang <jhuang9240@gmail.com>
2019-10-28 14:55:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,79 @@
 # My Code here....
+def map_to_negativize(array)
+  i = 0
+  result = []
+  while i < array.length do
+    result[i] = array[i] * -1
+    i += 1
+  end
+  result
+end
+
+def map_to_no_change(array)
+  i = 0
+  result = []
+  while i < array.length do
+    result[i] = array[i]
+    i += 1
+  end
+  result
+end
+
+def map_to_double(array)
+  i = 0
+  result = []
+  while i < array.length do
+    result[i] = array[i] * 2
+    i += 1
+  end
+  result
+end
+
+def map_to_square(array)
+  i = 0
+  result = []
+  while i < array.length do
+    result[i] = array[i] * array[i]
+    i += 1
+  end
+  result
+end
+
+def reduce_to_total(array, start = 0)
+  i = 0
+  total = start
+  while i < array.length do
+    total += array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(array)
+  i = 0
+  total = 0
+
+  while i < array.length do
+    total += array[i]
+    result = false
+  end
+  if total.is_a? integer
+    total = true
+    i += 1
+  end
+  result
+end
+
+def reduce_to_all_false(array)
+  i = 0
+  total = 0
+  result = true
+  while i < array.length do
+    total += array[i]
+    i += 1
+  end
+  if total.is_a? integer
+    result = false
+  end
+  result
+end
